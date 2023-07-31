edafe612a2c3a5ebc31dd98444544587d8d927b5
laogede <2912842867@qq.com>
2023-07-31 15:46:38 +0800

    Remove unused code

--- test/textrich_method_channel_test.dart
+++ test/textrich_method_channel_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:textrich/textrich_method_channel.dart';
 
 void main() {
-  MethodChannelTextrich platform = MethodChannelTextrich();
   const MethodChannel channel = MethodChannel('textrich');
 
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +16,4 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
 }
